f254c0ab6c98a2a5da4f7e30d269fb338f2e7c10
thanhnguyen <ndt181@gmail.com>
2018-04-30 18:35:40 +0200

    crm folder

--- crm/main.js
+++ crm/main.js
@@ -1,53 +1,147 @@
 
-var _table_ = document.createElement('table'),
-_tr_ = document.createElement('tr'),
-_th_ = document.createElement('th'),
-_td_ = document.createElement('td');
 
-var NEW_LEAD_API = "https://script.google.com/macros/s/AKfycbydo_5UrWzxhIusxpcqcZ6JdN_U4prpVLlywqQTfjc/dev?key=abc&action=newlead";
+const app = function () {
+  const NEW_LEAD_API = "https://script.google.com/macros/s/AKfycbydo_5UrWzxhIusxpcqcZ6JdN_U4prpVLlywqQTfjc/dev?key=abc&action=newlead";
+  const API_BASE = 'https://script.google.com/macros/s/AKfycby-jPqmlNjRwcemO1v0k6UX9uuD2CLXdpcQQFKoc85g7hLSGI1R/exec';
+  const API_KEY = 'abc';
+  const CATEGORIES = ['general', 'financial', 'technology'];
 
+  const state = {activePage: 1, activeCategory: null};
+  const page = {};
 
-// Builds the HTML Table out of myList json data from Ivy restful service.
- function buildHtmlTable(arr) {
-     var table = _table_.cloneNode(false),
-         columns = addAllColumnHeaders(arr, table);
-     for (var i=0, maxi=arr.length; i < maxi; ++i) {
-         var tr = _tr_.cloneNode(false);
-         for (var j=0, maxj=columns.length; j < maxj ; ++j) {
-             var td = _td_.cloneNode(false);
-                 cellValue = arr[i][columns[j]];
-             td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
-             tr.appendChild(td);
-         }
-         table.appendChild(tr);
-     }
-     return table;
- }
- 
- // Adds a header row to the table and returns the set of columns.
- // Need to do union of keys from all records as some records may not contain
- // all records
- function addAllColumnHeaders(arr, table)
- {
-     var columnSet = [],
-         tr = _tr_.cloneNode(false);
-     for (var i=0, l=arr.length; i < l; i++) {
-         for (var key in arr[i]) {
-             if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key)===-1) {
-                 columnSet.push(key);
-                 var th = _th_.cloneNode(false);
-                 th.appendChild(document.createTextNode(key));
-                 tr.appendChild(th);
-             }
-         }
-     }
-     table.appendChild(tr);
-     return columnSet;
- }
+  function init () {
+    // page.notice = document.getElementById('notice');
+    // page.filter = document.getElementById('filter');
+    // page.container = document.getElementById('container');
 
+    // _buildFilter();
+    _getNewPosts();
+  }
 
-document.body.appendChild(buildHtmlTable([
-    {"name" : "abc", "age" : 50},
-    {"age" : "25", "hobby" : "swimming"},
-    {"name" : "xyz", "hobby" : "programming"}
-]));
+  function _getNewPosts () {
+    // page.container.innerHTML = '';
+    _getPosts();
+  }
+
+  function _getPosts () {
+    // _setNotice('Loading posts');
+
+    fetch(NEW_LEAD_API)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.status !== 'success') {
+          // _setNotice(json.message);
+          alert(json.message);
+        }
+
+        _renderPosts(json.data);
+        _renderPostsPagination(json.pages);
+      })
+      .catch((error) => {
+        // _setNotice('Unexpected error loading posts');
+      })
+  }
+
+  function _buildFilter () {
+      page.filter.appendChild(_buildFilterLink('no filter', true));
+
+      CATEGORIES.forEach(function (category) {
+        page.filter.appendChild(_buildFilterLink(category, false));
+      });
+  }
+
+  function _buildFilterLink (label, isSelected) {
+    const link = document.createElement('button');
+      link.innerHTML = _capitalize(label);
+      link.classList = isSelected ? 'selected' : '';
+      link.onclick = function (event) {
+        let category = label === 'no filter' ? null : label.toLowerCase();
+
+      _resetActivePage();
+        _setActiveCategory(category);
+        _getNewPosts();
+      };
+
+      return link;
+  }
+
+  function _buildApiUrl (page, category) {
+    let url = API_BASE;
+    url += '?key=' + API_KEY;
+    url += '&page=' + page;
+    url += category !== null ? '&category=' + category : '';
+
+    return url;
+  }
+
+  function _setNotice (label) {
+    // page.notice.innerHTML = label;
+  }
+
+  function _renderPosts (posts) {
+    posts.forEach(function (post) {
+      const article = document.createElement('article');
+      article.innerHTML = `
+        <h2>${post.title}</h2>
+        <div class="article-details">
+          <div>By ${post.author} on ${_formatDate(post.timestamp)}</div>
+          <div>Posted in ${post.category}</div>
+        </div>
+        ${_formatContent(post.content)}
+      `;
+      page.container.appendChild(article);
+    });
+  }
+
+  function _renderPostsPagination (pages) {
+    if (pages.next) {
+      const link = document.createElement('button');
+      link.innerHTML = 'Load more posts';
+      link.onclick = function (event) {
+        _incrementActivePage();
+        _getPosts();
+      };
+
+      page.notice.innerHTML = '';
+      page.notice.appendChild(link);
+    } else {
+      _setNotice('No more posts to display');
+    }
+  }
+
+  function _formatDate (string) {
+    return new Date(string).toLocaleDateString('en-GB');
+  }
+
+  function _formatContent (string) {
+    return string.split('\n')
+      .filter((str) => str !== '')
+      .map((str) => `<p>${str}</p>`)
+      .join('');
+  }
+
+  function _capitalize (label) {
+    return label.slice(0, 1).toUpperCase() + label.slice(1).toLowerCase();
+  }
+
+  function _resetActivePage () {
+    state.activePage = 1;
+  }
+
+  function _incrementActivePage () {
+    state.activePage += 1;
+  }
+
+  function _setActiveCategory (category) {
+    state.activeCategory = category;
+    
+    const label = category === null ? 'no filter' : category;
+    Array.from(page.filter.children).forEach(function (element) {
+        element.classList = label === element.innerHTML.toLowerCase() ? 'selected' : '';
+      });
+  }
+
+  return {
+    init: init
+  };
+}();
