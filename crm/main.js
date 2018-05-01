c9af965abfafe4d19e9c1a05e01d9e8cbdf63cec
thanhnguyen <ndt181@gmail.com>
2018-05-01 02:38:30 +0200

    crm folder

--- crm/main.js
+++ crm/main.js
@@ -1,7 +1,8 @@
 
 
 const app = function () {
-  const NEW_LEAD_API = "https://script.google.com/macros/s/AKfycbydo_5UrWzxhIusxpcqcZ6JdN_U4prpVLlywqQTfjc/exec?key=abc&action=newlead";
+  // https://script.google.com/macros/s/AKfycbwzFYIniU3IOaIBW6e54UQmxe3pkBQOpXI8idwn_jipA-hU01k/exec
+  const NEW_LEAD_API = "https://script.google.com/macros/s/AKfycbwzFYIniU3IOaIBW6e54UQmxe3pkBQOpXI8idwn_jipA-hU01k/exec?key=abc&action=newlead&callback=?";
   const API_BASE = 'https://script.google.com/macros/s/AKfycby-jPqmlNjRwcemO1v0k6UX9uuD2CLXdpcQQFKoc85g7hLSGI1R/exec';
   const API_KEY = 'abc';
   const CATEGORIES = ['general', 'financial', 'technology'];
