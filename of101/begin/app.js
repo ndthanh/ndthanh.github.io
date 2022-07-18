da00c3554112fc521b269ccfa08d776c4ea8a449
Nguyen Duc Thanh <dtnguyen@Nguyens-MacBook-Air.local>
2022-07-18 15:03:48 +0700

    update template in app.js

--- of101/begin/app.js
+++ of101/begin/app.js
@@ -1,28 +1,140 @@
 const app = Vue.createApp({
-  data() {
-    return {}
+  components: {},
+
+  setup() {
+    const { onMounted, ref } = Vue
+    const $q = Quasar.useQuasar()
+    const leftDrawerOpen = ref(false)
+
+    onMounted(async () => { })
+
   },
-  methods: {
-    onSetColor() {
-      window.Excel.run(async context => {
-        const range = context.workbook.getSelectedRange();
-        range.format.fill.color = 'green';
-        await context.sync();
-      });
-    },
-    NOTIFY(msg, type = 1) {
-      this.$q.notify({
-        message: msg,
-        progress: true,
-        type: type == 1 ? 'positive' : 'negative',
-        color: type == 1 ? 'green' : 'red',
-        position: 'top',
-        timeout: 2000
-      })
-    }
-  },
-  created() { },
-  mounted() { }
+  template: `
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Học Excel Online
+        </q-toolbar-title>
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://quasar.dev"
+        >
+          <q-item-section avatar>
+            <q-icon name="school" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Docs</q-item-label>
+            <q-item-label caption>quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://github.com/quasarframework/"
+        >
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Github</q-item-label>
+            <q-item-label caption>github.com/quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://chat.quasar.dev"
+        >
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Discord Chat Channel</q-item-label>
+            <q-item-label caption>chat.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://forum.quasar.dev"
+        >
+          <q-item-section avatar>
+            <q-icon name="forum" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Forum</q-item-label>
+            <q-item-label caption>forum.quasar.dev</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          tag="a"
+          target="_blank"
+          href="https://twitter.com/quasarframework"
+        >
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      This is where router view will come.
+    </q-page-container>
+  </q-layout>
+  `
+  // methods: {
+  //   onSetColor() {
+  //     window.Excel.run(async context => {
+  //       const range = context.workbook.getSelectedRange();
+  //       range.format.fill.color = 'green';
+  //       await context.sync();
+  //     });
+  //   },
+  //   NOTIFY(msg, type = 1) {
+  //     this.$q.notify({
+  //       message: msg,
+  //       progress: true,
+  //       type: type == 1 ? 'positive' : 'negative',
+  //       color: type == 1 ? 'green' : 'red',
+  //       position: 'top',
+  //       timeout: 2000
+  //     })
+  //   }
+  // }
 })
 
 window.Office.onReady(() => {
