9725c9aad7fbfd8b5a883654e9ed3c7888bb2195
Nguyen Duc Thanh <dtnguyen@Nguyens-MacBook-Air.local>
2022-07-18 16:13:08 +0700

    ok

--- of101/begin/router.js
+++ of101/begin/router.js
@@ -1,12 +1,10 @@
 import home from './pages/home.js'
 import page1 from './pages/page1.js'
-// import page2 from './pages/page2.js'
 
 export default VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes: [
-    { path: '/', component: home },
-    { path: '/page1', component: page1 },
-    // { path: '/page2', component: page2 }
+    { path: '/', component: home, name: 'Home' },
+    { path: '/page1', component: page1, name: 'Page1' }
   ]
-})
+});
