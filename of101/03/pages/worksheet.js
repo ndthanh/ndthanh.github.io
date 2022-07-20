f6fcc12df0f74307c73d6caa81c7bc5d21e8b415
Nguyen Duc Thanh <dtnguyen@Nguyens-MacBook-Air.local>
2022-07-20 15:34:21 +0700

    add /03 add change event handler

--- of101/03/pages/worksheet.js
+++ of101/03/pages/worksheet.js
@@ -110,11 +110,92 @@ export default {
 
     }
 
+    const ndt_AddSheet = () => {
+
+      window.Excel.run(async (context) => {
+        let sheets = context.workbook.worksheets
+
+        let sheet = sheets.add("Sample")
+        sheet.load("name, position")
+
+        await context.sync()
+        log.value = `Added worksheet named "${sheet.name}" in position ${sheet.position}`
+        Quasar.Notify.create(log.value)
+      });
+
+    }
+
+    const ndt_CopySheet = () => {
+      window.Excel.run(async (context) => {
+        let myWorkbook = context.workbook;
+        let sampleSheet = myWorkbook.worksheets.getActiveWorksheet();
+        let copiedSheet = sampleSheet.copy(Excel.WorksheetPositionType.after, sampleSheet);
+        await context.sync();
+      });
+    }
+
+    const ndt_DeleteLastSheet = () => {
+
+      window.Excel.run(async (context) => {
+        let sheets = context.workbook.worksheets;
+        sheets.load("items/name")
+
+        await context.sync()
+        if (sheets.items.length === 1) {
+          log.value = "Unable to delete the only worksheet in the workbook"
+          Quasar.Notify.create(log.value)
+        } else {
+          let lastSheet = sheets.items[sheets.items.length - 1];
+
+          log.value = `Deleting worksheet named "${lastSheet.name}"`
+          Quasar.Notify.create(log.value)
+          lastSheet.delete()
+
+          await context.sync()
+        }
+      })
+
+    }
+
+    const ndt_RenameSheet = () => {
+      window.Excel.run(async (context) => {
+        let currentSheet = context.workbook.worksheets.getActiveWorksheet();
+        currentSheet.name = "New Name";
+
+        await context.sync();
+      });
+    }
+
+    const ndt_add_event = () => {
+      window.Excel.run(async (context) => {
+        let sheet = context.workbook.worksheets.getItem("Sheet1")
+        sheet.onChanged.add(onWorksheetChanged)
+        await context.sync()
+
+        log.value = "Added a worksheet-level data-changed event handler."
+        Quasar.Notify.create(log.value)
+      })
+    }
+
+    function onWorksheetChanged(eventArgs) {
+      window.Excel.run(function (context) {
+        let details = eventArgs.details;
+        let address = eventArgs.address;
+
+        // Print the before and after types and values to the console.
+        log.value = `Change at ${address}: was ${details.valueBefore}(${details.valueTypeBefore}),`
+          + ` now is ${details.valueAfter}(${details.valueTypeAfter})`
+        return context.sync();
+      });
+    }
+
     return {
       title, counterStore, log,
       ndt_GetSheetInfo, ndt_GetActiveSheet, ndt_SetActiveSheet,
       ndt_GetFirstSheet, ndt_GetLastSheet,
-      ndt_GetNextSheet, ndt_GetPreviousSheet
+      ndt_GetNextSheet, ndt_GetPreviousSheet,
+      ndt_AddSheet, ndt_CopySheet, ndt_DeleteLastSheet,
+      ndt_RenameSheet, ndt_add_event
     }
   },
 
@@ -182,6 +263,41 @@ export default {
             label="Get Previous Sheet"
             @click="ndt_GetPreviousSheet"
           />
+          
+          <q-btn
+            push
+            label="Add Sheet"
+            @click="ndt_AddSheet"
+          />
+
+          <q-btn
+            push
+            label="Copy Active Sheet"
+            @click="ndt_CopySheet"
+          />
+
+          <q-btn
+            push
+            label="Delete Last Sheet"
+            @click="ndt_DeleteLastSheet"
+          />
+
+          <q-btn
+            push
+            label="Rename current Sheet"
+            @click="ndt_RenameSheet"
+          />
+
+          <q-separator />
+
+          <p class="text-subtitle1">Change event</p>
+
+          <q-btn
+            push
+            label="Add change event handler to Sheet1"
+            @click="ndt_add_event"
+          />
+
           <q-separator />
 
         </div>
